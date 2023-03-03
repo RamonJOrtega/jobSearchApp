77296f83a9dc3c685a7c46e59522fea5e2604c88
Ramon Ortega <ramon.justis.ortega@gmail.com>
2023-03-02 16:09:30 -0800

    change axiosHelpersto tsx from js (#15)

---
+++ job-search-web-app/README.md
@@ -0,0 +1,28 @@
+# Job Search App
+This web-based application makes calls to the free API at [4dayweek.io](https://4dayweek.io/).
+The app lists all current companies offering positions with 4 day work weeks. (Usually over 100 jobs listed).
+The home page fetches all companies with job openints to show a table of all company links sorted by most openings to least openings.
+The dynamic route pages fetch the respective company icon and job title to show tables of all job titles links available at that company.
+The app uses node, express, ejs, and axios dpendencies listed in the [package.json file](https://github.com/RamonJOrtega/jobSearchApp/blob/main/package.json).
+
+
+## To Run the App
+1. Make sure node and npm are installed.
+2. Clone the repository to your machine, and and navigate to that directory.
+3. Install dependencies by running `npm install` in the terminal.
+4. Run `node index.js` in the terminal.
+5. Open a browser and navigate to http://localhost:8001/.
+
+
+### Home Page Shown Below
+![alt text](https://github.com/RamonJOrtega/jobSearchApp/blob/main/public/img/appHome1.png)
+
+
+
+### Home Page Instant Search Box Shown Below
+![alt text](https://github.com/RamonJOrtega/jobSearchApp/blob/main/public/img/appHome.png)
+
+
+
+### Company Result Page ShownBelow
+![alt text](https://github.com/RamonJOrtega/jobSearchApp/blob/main/public/img/appHome3.png)
---
+++ job-search-web-app/helpers/sortingFunctions.js
@@ -0,0 +1,50 @@
+ const getFirstColFrmTwoColArray = (twoColArray) => {
+    let oneColArray = [];
+    for (let i = 0 ; i < twoColArray.length; i++) {oneColArray[i] = twoColArray[i][0]}
+    return oneColArray;
+}
+
+ const listCompanies = (array) => {
+    let companyArray=[];
+    for (let i = 0; i < array.length; i++) {companyArray[i] = array[i].company_name}
+    return companyArray;
+}
+ 
+ const companyFrequency = (jobArray) => {
+    let counter = {};
+    for (const element of jobArray) {
+        if (counter[element]) {counter[element] += 1;} 
+        else {counter[element] = 1;}
+    };
+    const freqArray = Object.entries(counter);
+    return freqArray;
+}
+
+ const sortFreqLargeToSmall = (freqArray) => {
+    return freqArray.sort((a,b) => b[1]-a[1]);
+}
+
+ const findCompanyJobs = (company, jobObject) => {
+    let jobArray = [];
+    for (let i=0; i < jobObject.length; i++) {
+        if (company == jobObject[i].company_name) {
+        jobArray.push([
+            jobObject[i].title, 
+            jobObject[i].company.country,
+            jobObject[i].company.remote_level,
+            jobObject[i].company.logo_url,
+            jobObject[i].url
+        ]);
+        } 
+    }
+    console.log(jobArray);
+    return jobArray;
+}
+
+module.exports = {
+    getFirstColFrmTwoColArray,
+    listCompanies,
+    companyFrequency,
+    sortFreqLargeToSmall,
+    findCompanyJobs,
+};
